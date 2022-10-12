8505f902fc2cd2f3be1a435d2fc4c453515f00ed
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-10-12 15:21:01 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -12,12 +12,6 @@ from io import BytesIO
 import requests
 import urllib.request
 
-# Data to be written to Deta Base
-with st.form("form"):
-    name = st.text_input("Your name")
-    age = st.number_input("Your age")
-    submitted = st.form_submit_button("Store in database")
-
 def _font_as_bytes():
     with open('https://raw.githubusercontent.com/wlyi1/random/main/Random/Quicksand-Regular.ttf', 'rb') as f:
         font_bytes = BytesIO(f.read())
@@ -28,8 +22,12 @@ image3 = Image.open(BytesIO(resp.content))
 
 #Data Sources
 data = pd.read_csv('rand_aktivitas.csv')
-image1 = 'https://raw.githubusercontent.com/wlyi1/random/main/Random/a3a.png'
+image2 = 'https://raw.githubusercontent.com/wlyi1/random/main/Random/a3a.png'
+image1 = 'https://raw.githubusercontent.com/wlyi1/random/main/Random/a2.png'
 st.image(image1)
+st.image(image2)
+#stwrite
+
 list_rand = data.name
 tgl_random = datetime.datetime.now()
 tgl = tgl_random.strftime("%m/%d/%Y, %H:%M:%S")
@@ -45,25 +43,39 @@ img.text((80,470), today_rand, font=font, fill=(0,0,0))
 img.text((450,390), hari, font=font1, fill=(0,0,0))
 
 
+# Connect to Deta Base with your Project Key
 deta = Deta(st.secrets["project_key"])
 
-# Create a new database "example-db"
+# Create a new database "example-db sss"
 # If you need a new database, just use another name.
 db = deta.Base("random")
 
-# If the user clicked the submit button,
-# write the data from the form to the database.
-# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
-if submitted:
+if st.button('Show'):
     st.image(image3)
     db.put({'Tanggal' : tgl, 'Random' : today_rand})
-    #db.put({"names": name, "ages": age})
 
-"---"
-"Here's everything stored in the database:"
-# This reads all items from the database and displays them to your app.
-# db_content is a list of dictionaries. You can do everything you want with it.
+
+# Data to be written to Deta Base of Cerita
+db1 = deta.Base('Cerita')
+with st.form("my_form"):
+    st.write("Ceritain ke RandomKu dong tentang aktivitas randommu hari ini 😃")
+    nama = st.text_input("Namanya? ")
+    age = st.number_input('Umurnya? ', min_value=5, max_value=70)
+    cerita = st.text_input("Cerita singkatnya gimana nih?")
+    submitted = st.form_submit_button("Submit")
+
+# If the user clicked the submit button. write the data from the form to the database.
+# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+
+if submitted:
+    db1.put({"Nama": nama, 'Umur' : age, "Cerita": cerita})
+
+st.write('Test Your Knowledge')
+
 db_content = db.fetch().items
 st.write(db_content)
 df = pd.DataFrame(db_content)
-st.dataframe(df)
+st.write(df)
+st.write(df['Random'][0])
+
+
