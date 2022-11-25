f3e6b1dd80e4cd256f0f4e7c4e1a6fa5975dd535
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-11-25 16:40:42 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -11,11 +11,11 @@ db = firestore.Client(credentials=creds, project="testrandom1-6cf06")
 
 st.subheader("Punya ide aktivitas random? ✍️")
     #st.caption('tulisin disini, bebas apa aja!')
-    colide = db.collection('list')
-    with st.form("ide randommu", clear_on_submit=True):
-        #st.write("tulisin apa aja ya bebas!")
-        ide = st.text_input('')
-        sub = st.form_submit_button('Kirim')
-        if sub:
-            colide.add({'name': ide})
-            st.write('makasih idenya ya')
+colide = db.collection('list')
+with st.form("ide randommu", clear_on_submit=True):
+    #st.write("tulisin apa aja ya bebas!")
+    ide = st.text_input('')
+    sub = st.form_submit_button('Kirim')
+    if sub:
+        colide.add({'name': ide})
+        st.write('makasih idenya ya')
